a97de68970e8141c751e9a74cc678965a80bf2d4
DhumPotashPermanganate <39737799+DhumPotashPermanganate@users.noreply.github.com>
2021-03-31 03:10:23 +0530

    Stable version

--- src/Employer.java
+++ src/Employer.java
@@ -1,2 +1,25 @@
-public class Employer {
+public class Employer 
+{
+    Worker worker= new Worker();
+    public double Fed=0, Loc=0, Stat=0, wag=0, ded=0, netp=0;
+    <worker> void colectDataForEmployerReport(worker w)
+    {
+        Fed= Fed+ worker.FederalT;
+        Loc= Loc+ worker.LocalT;
+        Stat= Stat+ worker.StateT;
+        wag= wag+worker.TotalSal;
+        ded= ded+worker.netdeduct;
+        netp= wag-ded;
+    }
+
+    void printDataForEmployerReport()
+    {
+        System.out.println("\t\tCurrent");
+        System.out.println("Federal\t\t"+Fed);
+        System.out.println("State\t\t"+Stat);
+        System.out.println("Local\t\t"+Loc);
+        System.out.println("Gross wages:\t\t\t\t"+wag);
+        System.out.println("Total Deductions:\t\t\t\t"+ded);
+        System.out.println("Net Pay: \t\t\t\t"+netp);
+    }
 }
